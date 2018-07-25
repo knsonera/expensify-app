b252708e814dbb1cff4b20ea091d875a587af799
Alina <kn.sonera@gmail.com>
2018-07-24 17:02:14 -0700

    new: redux integration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+const expenseOne = store.dispatch(addExpense( { description: 'Water bill', amount: 4500, createdAt: -1000} ));
+const expenseTwo = store.dispatch(addExpense( { description: 'Gas bill', amount: 1000, createdAt: 1000} ));
+const expenseThree = store.dispatch(addExpense( { description: 'Rent', amount: 100950, createdAt: 0} ));
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
