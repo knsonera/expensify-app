a3895611a6ceed25a6bc152c196311822411fe56
Alina <kn.sonera@gmail.com>
2018-07-24 17:03:02 -0700

    new: expenses list

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
+
+const ExpenseList = (props) => (
+    <div>
+        <h2>Expense List</h2>
+        There are {props.expenses.length} expense(s):
+        {
+            props.expenses.map((expense) => {
+                return <ExpenseListItem key={expense.id} {...expense}/>
+            })
+        }
+    </div>
+);
+
+const mapStateToProps = (state) => {
+    return {
+        expenses: selectExpenses(state.expenses, state.filters)
+    };
+};
+
+export default connect(mapStateToProps)(ExpenseList);
